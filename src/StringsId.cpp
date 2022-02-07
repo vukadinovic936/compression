37c6dc315aeedb21bb0070f566f0a12e66fa45bb
vukadinovic936 <milosvuk99@gmail.com>
2022-02-07 14:14:02 +0200

    keep hierarchy

--- src/StringsId.cpp
+++ src/StringsId.cpp
@@ -3,4 +3,4 @@ const char* STRINGS_COMPRESS_COMMAND="compress";
 const char* STRINGS_UNCOMPRESS_COMMAND="uncompress";
 const char* STRINGS_HELP_COMMAND="help";
 const char* STRINGS_ALGOS_COMMAND="algos";
-const char* STRINGS_HELP_CONTENT = "zeno compress filename.txt		compress the file \n zeno uncompress filename.zen		uncompress the file\n zeno algos		display algorithms"
+const char* STRINGS_HELP_CONTENT = "zeno compress filename.txt		compress the file \n zeno uncompress filename.zen		uncompress the file\n zeno algos		display algorithms";
